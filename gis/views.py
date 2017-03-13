791b2cc70cea6b970e546b11c9872d95ef95f8dd
Anna Umanets <ann4193@gmail.com>
2017-03-13 12:52:08 +0200

    Articles, urls, views were added

--- gis/views.py
+++ gis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from gis.models import Articles
 
@@ -6,11 +6,15 @@ from gis.models import Articles
 def home(request):
     #return HttpResponse("Hello world")
     #return render(request, 'gis/base.html')
-    articles = Articles.object.all()
-    contex = {
+    articles = Articles.objects.all()
+    context = {
         'articles': articles
     }
-    return render(request, 'gis/base.html', context)
+    return render(request, 'gis/home.html', context)
 
 def about(request):
     return render(request, 'gis/about.html')
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'gis/article.html', {'article': article})
