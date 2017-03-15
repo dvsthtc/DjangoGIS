bea8301bc04236122939f5ae7521780aaa7424b5
Anna Umanets <ann4193@gmail.com>
2017-03-15 23:52:19 +0200

    Created Map view

--- gis/views.py
+++ gis/views.py
@@ -18,3 +18,6 @@ def about(request):
 def show_articles(request, article_id):
     article = get_object_or_404(Articles, id=article_id)
     return render(request, 'gis/article.html', {'article': article})
+
+def mymap(request):
+    return render(request, 'gis/map.html')
